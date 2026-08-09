57a23782e062bafda912ec071c7e89714768f31c
Vitor A Pantoja <vitorpantoja.dev@gmail.com>
2026-08-09 09:27:46 -0300

    Shorten compromisso title display length

--- js/home.js
+++ js/home.js
@@ -284,8 +284,8 @@ async function renderizarSemana() {
                 const item = document.createElement("div");
                 item.className = "item-semana";
                 item.dataset.id = compromisso.id;
-                const tituloCurto = compromisso.titulo.length > 12
-                    ? compromisso.titulo.slice(0, 12) + "…"
+                const tituloCurto = compromisso.titulo.length > 8
+                    ? compromisso.titulo.slice(0, 8) + "…"
                     : compromisso.titulo;
                 item.innerHTML = tituloCurto;
                 item.title = compromisso.titulo;
